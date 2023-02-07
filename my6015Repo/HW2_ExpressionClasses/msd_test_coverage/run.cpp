a7d7bcb746e6d7f9ce4dfa89b3bef17d3c70548c
Jinny Jeng <j2695203@gmail.com>
2023-02-06 23:07:04 -0700

    finish HW2

---
+++ my6015Repo/HW2_ExpressionClasses/msd_test_coverage/run.cpp
@@ -0,0 +1,8 @@
+//
+//  run.cpp
+//  msd_test_coverage
+//
+//  Created by Jinny Jeng on 2/2/23.
+//
+
+#include "run.hpp"
---
+++ my6015Repo/HW2_ExpressionClasses/msd_test_coverage/run.hpp
@@ -0,0 +1,13 @@
+//
+//  run.hpp
+//  msd_test_coverage
+//
+//  Created by Jinny Jeng on 2/2/23.
+//
+
+#ifndef run_hpp
+#define run_hpp
+
+#include <stdio.h>
+
+#endif /* run_hpp */
