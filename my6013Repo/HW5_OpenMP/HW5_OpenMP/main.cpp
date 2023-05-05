bc0c5213ff57a2e09150fac67c8d2c5341240bb9
Jinny Jeng <j2695203@gmail.com>
2023-05-05 16:58:05 +0800

    finish 6014HW Encfs

---
+++ my6013Repo/HW5_OpenMP/HW5_OpenMP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW5_OpenMP
+//
+//  Created by Jinny Jeng on 4/20/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
